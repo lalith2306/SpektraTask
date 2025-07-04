2e99bc9cd83bf671a267f138922bafee307f875c
LALITH KUMAR B S <127776729+lalith2306@users.noreply.github.com>
2025-07-04 09:24:29 +0530

    Update run_script.bat

--- run_script.bat
+++ run_script.bat
@@ -2,8 +2,21 @@
 set USERNAME=badactor
 set PASSWORD=%1
 set EDIT_SCRIPT_URL=%2
-"C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10\Downloads\0\create_user_and_ssh.ps1" -username %USERNAME% -password %PASSWORD% -editScriptUrl %EDIT_SCRIPT_URL%
+set PWSH="C:\Program Files\PowerShell\7\pwsh.exe"
+set SCRIPT_DIR=C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10\Downloads\0
+set SCRIPT=%SCRIPT_DIR%\create_user_and_ssh.ps1
+echo Checking paths >> C:\ProgramData\SSHSetupLog.txt
+if not exist %PWSH% (
+    echo PowerShell 7 not found at %PWSH% >> C:\ProgramData\SSHSetupLog.txt
+    exit /b 3
+)
+if not exist %SCRIPT% (
+    echo Script not found at %SCRIPT% >> C:\ProgramData\SSHSetupLog.txt
+    exit /b 3
+)
+echo Executing %SCRIPT% >> C:\ProgramData\SSHSetupLog.txt
+%PWSH% -ExecutionPolicy Bypass -File %SCRIPT% -username %USERNAME% -password %PASSWORD% -editScriptUrl %EDIT_SCRIPT_URL%
 if %ERRORLEVEL% NEQ 0 (
-    echo Failed to execute PowerShell script >> C:\ProgramData\SSHSetupLog.txt
+    echo Failed to execute PowerShell script with error %ERRORLEVEL% >> C:\ProgramData\SSHSetupLog.txt
     exit /b %ERRORLEVEL%
 )
