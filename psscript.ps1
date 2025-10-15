9072b88cf821f1d0c1b7ca2da73129d8c92782f2
LALITH KUMAR B S <127776729+lalith2306@users.noreply.github.com>
2025-10-15 17:16:44 +0530

    Update psscript.ps1

--- psscript.ps1
+++ psscript.ps1
@@ -192,8 +192,6 @@ if ($provisionNonAdminUser -eq "yes" -and $vmNonAdminUserName -ne "") {
     Write-Host "Final Shadow target set to Admin: $vmUserToShadow"
 }
 
-$vmAdminUsername = $vmUserToShadow
-
 #Password reset for trainer if specialized image is used
 $updatedTrainerPassword = "$trainerUserPassword"
 
@@ -218,3 +216,4 @@ else {
 
 Stop-Transcript
 Restart-Computer -Force
+
