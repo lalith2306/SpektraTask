22fb07edf1e240459329fb128a50cc3b9f002ce7
LALITH KUMAR B S <127776729+lalith2306@users.noreply.github.com>
2025-12-01 20:13:00 +0530

    Update windows.ps1

--- VM/windows.ps1
+++ VM/windows.ps1
@@ -1,5 +1,23 @@
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
 
+$pssUrl = "https://experienceazure.blob.core.windows.net/vmaas/s/arm-templates/scripts/psscript.ps1"
+$functionsUrl = "https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/cloudlabs-windows-functions.ps1"
+
+Invoke-WebRequest -Uri $pssUrl -OutFile "$env:TEMP\psscript.ps1" -UseBasicParsing
+Invoke-WebRequest -Uri $functionsUrl -OutFile "$env:TEMP\cloudlabs-windows-functions.ps1" -UseBasicParsing
+
+# Run psscript.ps1 with exact same parameters that ARM would pass
+powershell -ExecutionPolicy Unrestricted -File "$env:TEMP\psscript.ps1" `
+    -trainerUserName "$env:TRAINERUSERNAME" `
+    -trainerUserPassword "$env:TRAINERUSERPASSWORD" `
+    -vmCustomImageOsState "$env:VMCUSTOMIMAGEOSSTATE" `
+    -vmAdminUserName "$env:VMADMINUSERNAME" `
+    -vmAdminPassword "$env:VMADMINPASSWORD" `
+    -provisionNonAdminUser "$env:PROVISIONNONADMINUSER" `
+    -vmNonAdminUserName "$env:VMNONADMINUSERNAME" `
+    -vmNonAdminPassword "$env:VMNONADMINPASSWORD" `
+    -vmImageType "$env:VMIMAGETYPE"
+    
 # Define the desired DNS server
 $dnsServer = "10.0.0.8"
 
@@ -21,3 +39,4 @@ New-LocalUser -Name "RecastUser" -Password $password  -Description "New Administ
 Add-LocalGroupMember -Group "Administrators" -Member "RecastUser"
 
 Stop-Transcript
+
