1202500d29632b0c2ade13455fe2e5738f93965b
LALITH KUMAR B S <127776729+lalith2306@users.noreply.github.com>
2025-12-08 17:32:33 +0530

    Update rms.ps1

--- VM/rms.ps1
+++ VM/rms.ps1
@@ -1,5 +1,3 @@
-Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
-
 Param (
     [Parameter(Mandatory = $true)]
     [string]$trainerUserName,
@@ -12,6 +10,8 @@ Param (
     [string]$vmNonAdminPassword,
     [string]$vmImageType
 )
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
 
 $pssUrl = "https://experienceazure.blob.core.windows.net/vmaas/s/arm-templates/scripts/psscript.ps1"
 $functionsUrl = "https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/cloudlabs-windows-functions.ps1"
@@ -52,6 +52,7 @@ New-LocalUser -Name "RecastAdmin" -Password $password  -Description "New Adminis
 Add-LocalGroupMember -Group "Administrators" -Member "RecastAdmin"
 
 Stop-Transcript
+
 
 
 
