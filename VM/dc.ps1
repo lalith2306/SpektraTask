828e0025360ca8c5084d9bc70a2e548058131711
LALITH KUMAR B S <127776729+lalith2306@users.noreply.github.com>
2025-12-12 11:19:26 +0530

    Update dc.ps1

--- VM/dc.ps1
+++ VM/dc.ps1
@@ -1,5 +1,3 @@
-Start-Transcript -Path "C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt" -Append
-
 Param (
     [Parameter(Mandatory = $true)]
     [string]$trainerUserName,
@@ -12,6 +10,8 @@ Param (
     [string]$vmNonAdminPassword,
     [string]$vmImageType
 )
+
+Start-Transcript -Path "C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt" -Append
 
 $pssUrl = "https://experienceazure.blob.core.windows.net/vmaas/s/arm-templates/scripts/psscript.ps1"
 $functionsUrl = "https://experienceazure.blob.core.windows.net/templates/cloudlabs-common/cloudlabs-windows-functions.ps1"
@@ -102,3 +102,4 @@ powershell -ExecutionPolicy Unrestricted -File "$env:TEMP\psscript.ps1" `
 
 # After everything is done, stop transcript
 Stop-Transcript
+
